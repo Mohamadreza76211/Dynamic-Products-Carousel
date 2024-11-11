50d407532f7375f25f5d96a0f28058f1c8aa9a9a
Mohamadreza <najafi.mohamad2021@gmail.com>
2024-11-11 15:22:32 +0330

    Carousel

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,164 @@
-import Image from "next/image";
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
+import SwiperCore, { EffectCoverflow, Pagination } from "swiper";
+import { useSearchParams, useRouter } from "next/navigation";
 
-export default function Home() {
+SwiperCore.use([EffectCoverflow, Pagination]);
+
+const CurveSlider = ({ selectedCategory, cats }) => {
+  const [slides, setSlides] = useState([]);
+  const [visibleNames, setVisibleNames] = useState([]);
+  const [visibleUids, setVisibleUids] = useState([]);
+  const [selectedSlideIndex, setSelectedSlideIndex] = useState(2);
+  const [isClicked, setIsClicked] = useState(false);
+  const [displayedSlideIndex, setDisplayedSlideIndex] =
+    useState(selectedSlideIndex);
+  const [showNames, setShowNames] = useState(false);
+  const [backgroundColors, setBackgroundColors] = useState([]);
+
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const cid = searchParams.get("cid");
+  const mid = searchParams.get("mid");
+
+  const parentsCategories = cats.filter((cat) => !cat.parent);
+  const level1 = parentsCategories;
+
+  const level2 = [];
+  level1.forEach((l1) => {
+    if (l1.childes?.length > 0) {
+      l1.childes.forEach((l2) => {
+        level2.push({ ...l2, parent: l1 });
+      });
+    }
+  });
+
+  const level3 = [];
+  level2.forEach((l2) => {
+    if (l2.childes?.length > 0) {
+      l2.childes.forEach((l3) => {
+        level3.push({ ...l3, parent: l2 });
+      });
+    }
+  });
+
+  const getVisibleImagesV1 = () => {
+    const allImages = level1;
+    const visibleImages = allImages.map((c) => c.image);
+    const visibleNamesArray = allImages.map((c) => c.name);
+    const visibleUidsArray = allImages.map((c) => c.uid);
+    const backgroundColorsArray = allImages.map(
+      (c) => c.color_code || "transparent"
+    );
+
+    setVisibleNames(visibleNamesArray);
+    setVisibleUids(visibleUidsArray);
+    setBackgroundColors(backgroundColorsArray);
+
+    return visibleImages;
+  };
+
+  const swiperRef = useRef(null);
+
+  useEffect(() => {
+    const images = getVisibleImagesV1();
+    setSlides(images);
+  }, [cats]);
+
+  const handleSlideChange = (swiper) => {
+    const newIndex = swiper.realIndex;
+    setSelectedSlideIndex(newIndex);
+    setIsClicked(false);
+  };
+
+  const handleSlideClick = (index) => {
+    setSelectedSlideIndex(index);
+    setDisplayedSlideIndex(index);
+    setIsClicked(true);
+    setShowNames(true);
+
+    const catUid = visibleUids[index];
+    router.push(`/shop/search?cid=${catUid}&mid=${mid}`);
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+    <section
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Swiper
+        className="by"
+        ref={swiperRef}
+        effect="coverflow"
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={3}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2,
+          slideShadows: true,
+        }}
+        pagination={false}
+        loop={true}
+        initialSlide={0}
+        onSlideChange={handleSlideChange}
+        style={{ padding: "50px 0", width: "94%" }}
+      >
+        {slides?.map((slide, index) => (
+          <SwiperSlide
+            className="hello"
+            key={index}
+            onClick={() => handleSlideClick(index)}
+            style={{
+              backgroundImage: `url(${slide})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              backgroundColor: backgroundColors[index],
+              width: "358px",
+              height: "400px",
+              borderRadius: "10px",
+              boxShadow: "0 15px 50px rgba(0, 0, 0, 0.2)",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        ))}
+      </Swiper>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "-30px",
+        }}
+      >
+        {showNames &&
+          visibleNames.map((name, index) => (
+            <div
+              key={index}
+              style={{
+                padding: "10px",
+                color: "black",
+                textAlign: "center",
+                fontWeight: "bold",
+                display: displayedSlideIndex === index ? "block" : "none",
+              }}
+            >
+              {name}
+            </div>
+          ))}
+      </div>
+    </section>
   );
-}
+};
+
+export default CurveSlider;
